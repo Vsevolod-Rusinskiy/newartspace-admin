517120a2b5a1af42f8d8ceae604bf5114168110e
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-08-15 16:56:46 +0300

    refactor

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,5 @@
-export const validateFileSize = (value) => {
-  if (value && value.rawFile && value.rawFile.size > 1048576) {
+export const validateFileSize = ({ rawFile } = {}) => {
+  if (rawFile?.size > 1048576) {
     return 'Размер файла должен быть не более 1MB'
   }
-  return undefined
 }
