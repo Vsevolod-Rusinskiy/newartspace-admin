04f1633fed371352f0ffd2ffdc1ed3c8ba0e59ec
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-08-15 15:29:16 +0300

    feat: add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,19 @@ import dataProvider from './dataProvider'
 import { PaintingShow } from '../components/PaintingShow/PaintingShow'
 import { PaintingCreate } from '../components/PaintingCreate/PaintingCreate'
 import PaintingEdit from '../components/PaintingEdit/PaintingEdit'
+import russianMessages from 'ra-language-russian'
+import polyglotI18nProvider from 'ra-i18n-polyglot'
+
+const i18nProvider = polyglotI18nProvider(() => russianMessages, 'ru')
 
 // test flag
 
 export const App = () => (
-  <Admin authProvider={authProvider} dataProvider={dataProvider}>
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    i18nProvider={i18nProvider}
+  >
     <Resource
       name='paintings'
       list={PaintingList}
