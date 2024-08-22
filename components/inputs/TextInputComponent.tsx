3c1255d8d1b95c9a94f0107536dc7c364b165236
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-08-22 18:38:56 +0300

    fix extra space bug

--- components/inputs/TextInputComponent.tsx
+++ components/inputs/TextInputComponent.tsx
@@ -4,15 +4,7 @@ import { TextInputPropTypes } from '../../types/propTypes/'
 
 const TextInputComponent: React.FC<TextInputPropTypes> = React.memo(
   ({ source, label, validate }) => {
-    const trimSpaces = (value: string): string => value?.trim()
-    return (
-      <TextInput
-        source={source}
-        label={label}
-        validate={validate}
-        parse={trimSpaces}
-      />
-    )
+    return <TextInput source={source} label={label} validate={validate} />
   }
 )
 TextInputComponent.displayName = 'TextInputComponent'
