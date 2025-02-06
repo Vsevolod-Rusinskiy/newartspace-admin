a38838132796f0b4c27cf469088f80012d7aaa86
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2025-02-06 17:18:25 +0300

    feat: add search filter for artists by name
    Add SearchInput to ArtistList component to enable searching artists by name
    - Implement search filter with 'artistName' source
    - Add emoji icon to search input placeholder
    - Update list component to include filters
    - Enhance visual labels with emojis for better user experience

--- components/artists/ArtistList/ArtistList.tsx
+++ components/artists/ArtistList/ArtistList.tsx
@@ -4,22 +4,32 @@ import {
   List,
   NumberField,
   TextField,
+  SearchInput,
 } from 'react-admin'
 import { ImageField } from 'react-admin'
 
+const ArtistFilters = [
+  <SearchInput
+    key='name'
+    source='artistName'
+    placeholder='👨‍🎨 Поиск по имени художника'
+    alwaysOn
+  />,
+]
+
 export const ArtistList = (props) => {
   return (
-    <List {...props}>
+    <List {...props} filters={ArtistFilters}>
       <Datagrid rowClick='show'>
         <CloneButton />
-        <NumberField source='priority' label='Приоритет' />
-        <TextField source='id' label='id' />
+        <NumberField source='priority' label='⭐ Приоритет' />
+        <NumberField source='id' label='id' />
         <ImageField
           source='imgUrl'
-          label='Изображение художника'
+          label='🖼️ Изображение художника'
           sortable={false}
         />
-        <TextField source='artistName' label='Имя художника' />
+        <TextField source='artistName' label='👨‍🎨 Имя художника' />
       </Datagrid>
     </List>
   )
