5e60ad018cf97355ad10ae5ad297e5c3a181f799
Vsevolod-Rusinskiy <leto99999@gmail.com>
2025-05-14 09:05:33 +0300

    fix: enhance error handling in axios instance for JWT token refresh
    - Add handling for canceled requests in axios interceptor
    - Prevent multiple token refresh attempts for the same request
    - Implement logout functionality when token refresh fails or encounters an error
    - Ensure user is redirected to the login page upon logout

--- src/api/axiosInstance/axiosInstance.ts
+++ src/api/axiosInstance/axiosInstance.ts
@@ -12,19 +12,43 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
+    // Если запрос был отменен, просто проксируем ошибку
+    if (axios.isCancel(error)) return Promise.reject(error)
+
     // Проверяем наличие объекта response и data
     if (error.response?.data?.message === 'jwt expired') {
       const originalRequest = error.config
-      const newToken = await refreshJwt()
 
-      if (newToken) {
-        originalRequest.headers['Authorization'] = `Bearer ${newToken}`
-        return axiosInstance(originalRequest)
-      } else {
-        // Если не удалось обновить токен, выполняем выход
+      // Предотвращаем повторные попытки обновления токена для того же запроса
+      if (originalRequest._retry) {
+        // Если это повторная попытка, выполняем выход
         localStorage.removeItem('auth')
         delete axiosInstance.defaults.headers.common['Authorization']
         window.location.href = '/#/login'
+        return Promise.reject(error)
+      }
+
+      originalRequest._retry = true
+
+      try {
+        const newToken = await refreshJwt()
+
+        if (newToken) {
+          originalRequest.headers['Authorization'] = `Bearer ${newToken}`
+          return axiosInstance(originalRequest)
+        } else {
+          // Если не удалось обновить токен, выполняем выход
+          localStorage.removeItem('auth')
+          delete axiosInstance.defaults.headers.common['Authorization']
+          window.location.href = '/#/login'
+          return Promise.reject(error)
+        }
+      } catch (refreshError) {
+        // Если произошла ошибка при обновлении токена
+        localStorage.removeItem('auth')
+        delete axiosInstance.defaults.headers.common['Authorization']
+        window.location.href = '/#/login'
+        return Promise.reject(refreshError)
       }
     }
     // Добавим дополнительно обработку 401 статуса
