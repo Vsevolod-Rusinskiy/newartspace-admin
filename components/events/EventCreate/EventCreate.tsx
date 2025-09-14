9639a91c3a9fc64791eadcaa964b0e0e41312002
Vsevolod-Rusinskiy <leto99999@gmail.com>
2025-09-14 13:44:20 +0300

    feat: enhance EventCreate component with event photos selection

--- components/events/EventCreate/EventCreate.tsx
+++ components/events/EventCreate/EventCreate.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import {
   Create,
   SimpleForm,
@@ -6,31 +8,75 @@ import {
   ImageInput,
   ImageField,
   required,
+  SelectArrayInput,
 } from 'react-admin'
 import { RichTextInput } from 'ra-input-rich-text'
 import '../../../styles/customStyles.css'
 import { TextInputComponent } from '../../inputs'
 
 const requiredValidation = required('Это обязательное поле')
+const apiUrl = import.meta.env.VITE_APP_API_URL || 'https://back.newartspace.ru'
 
-export const EventCreate = () => (
-  <Create mutationMode='pessimistic'>
-    <SimpleForm>
-      <ImageInput
-        source='pictures'
-        label='Фото или Видео'
-        accept='image/*,video/*'
-      >
-        <ImageField source='src' title='title' />
-      </ImageInput>
-      <TextInput
-        source='title'
-        label='Заголовок'
-        validate={requiredValidation}
-      />
-      <TextInputComponent source='priority' label='Приоритет' />
-      <DateInput source='date' label='Дата' validate={requiredValidation} />
-      <RichTextInput source='content' label='Текст' />
-    </SimpleForm>
-  </Create>
-)
+export const EventCreate = () => {
+  const [eventPhotos, setEventPhotos] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}/event-photos?limit=1000`)
+      .then((response) => {
+        setEventPhotos(response.data.data)
+      })
+      .catch((error) => {
+        console.error('Ошибка при получении event-photos:', error)
+      })
+  }, [])
+
+  return (
+    <Create mutationMode='pessimistic'>
+      <SimpleForm>
+        <ImageInput
+          source='pictures'
+          label='Фото или Видео'
+          accept='image/*,video/*'
+        >
+          <ImageField source='src' title='title' />
+        </ImageInput>
+        <SelectArrayInput
+          source='eventPhotoIds'
+          label='Дополнительные фото из event-photos'
+          choices={eventPhotos.map((photo) => ({
+            id: photo.id,
+            name: photo.title || photo.imgUrl,
+          }))}
+          optionText={(choice) => (
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              {choice.imgUrl && (
+                <img
+                  src={choice.imgUrl}
+                  alt=''
+                  style={{
+                    width: 40,
+                    height: 40,
+                    objectFit: 'cover',
+                    marginRight: 8,
+                  }}
+                />
+              )}
+              {choice.title || choice.imgUrl}
+            </span>
+          )}
+          optionValue='id'
+          style={{ minWidth: '300px' }}
+        />
+        <TextInput
+          source='title'
+          label='Заголовок'
+          validate={requiredValidation}
+        />
+        <TextInputComponent source='priority' label='Приоритет' />
+        <DateInput source='date' label='Дата' validate={requiredValidation} />
+        <RichTextInput source='content' label='Текст' />
+      </SimpleForm>
+    </Create>
+  )
+}
